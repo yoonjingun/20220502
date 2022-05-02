13d1c705a064f5322582bfa414c56f1d499349df
dbswlsrjs <wlsrjsdlaekd@naver.com>
2022-05-02 09:48:11 +0900

    asdasd

---
+++ asdasd.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int A = 20;
+	cout << A + 3 << endl;
+	cout << A - 3 << endl;
+	cout << A * 3 << endl;
+	cout << A / 3 << endl;
+
+	float B = 20.1f;
+	cout << B + 1.1f << endl;
+	cout << B - 1.1f << endl;
+	cout << B / 1.1f << endl;
+	cout << B * 1.1f << endl;
+
+	cout << "гою╖" << endl;
+
+	
+
+
+	return 0;
+
+}
